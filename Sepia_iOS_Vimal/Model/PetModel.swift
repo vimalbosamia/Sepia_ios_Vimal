d12f6370c699d6d54ae0cf5f642a6c3aef184905
Vimal Bosamiya <vimalbosamia@gmail.com>
2022-11-15 20:03:23 +0530

    Added config condition to load data

--- Sepia_iOS_Vimal/Model/PetModel.swift
+++ Sepia_iOS_Vimal/Model/PetModel.swift
@@ -52,29 +52,3 @@ struct Welcome: Codable {
 struct Settings: Codable {
     let workHours: String
 }
-
-//struct RootClass : Codable {
-//
-//    let settings : Setting?
-//
-//    enum CodingKeys: String, CodingKey {
-//        case settings
-//    }
-//    init(from decoder: Decoder) throws {
-//        _ = try decoder.container(keyedBy: CodingKeys.self)
-//        settings = try Setting(from: decoder)
-//    }
-//}
-//
-//struct Setting : Codable {
-//
-//    let workHours : String?
-//
-//    enum CodingKeys: String, CodingKey {
-//        case workHours = "workHours"
-//    }
-//    init(from decoder: Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-//        workHours = try values.decodeIfPresent(String.self, forKey: .workHours)
-//    }
-//}
